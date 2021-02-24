88c35ce9f09f24be0e8a65f5ecd509eba5011a76
Museus <museus@protonmail.com>
2021-02-23 19:14:07 -0500

    Push up mod and README

---
+++ MinibossControl.lua
@@ -0,0 +1,108 @@
+ModUtil.RegisterMod("MinibossControl")
+--[[
+The default for each miniboss is 1.
+For most "vanilla" results, make each biome
+add up to the number of minibosses there normally are.
+For example if you remove Barge you should add a Witches
+or Power Couple.
+]]--
+local config = {
+
+    Tartarus_Bombers = 1,
+    Tartarus_Doomstone = 1,
+    Tartarus_Sneak = 1,
+
+    Asphodel_Barge = 0,
+    Asphodel_PowerCouple = 1,
+    Asphodel_Witches = 2,
+
+    Elysium_ButterflyBall = 1,
+    Elysium_Asterius = 1,
+
+    -- If true, Tiny Vermin will not spawn
+    RemoveTinyVermin = true,
+}
+MinibossControl.config = config
+
+ModUtil.LoadOnce( function()
+    ModUtil.MapSetTable(RoomSetData, {
+        -- [[ Tartarus Miniboss Counts ]]
+        Tartarus = {
+            A_MiniBoss01 = {
+                MaxCreationsThisRun = config.Tartarus_Bombers,
+            },
+            A_MiniBoss02 = {
+                -- Middle Management Doomstone
+                MaxCreationsThisRun = config.Tartarus_Doomstone,
+            },
+            A_MiniBoss03 = {
+                MaxCreationsThisRun = config.Tartarus_Sneak,
+            },
+            A_MiniBoss04 = {
+                -- Vanilla Doomstone
+                MaxCreationsThisRun = config.Tartarus_Doomstone,
+            },
+        },
+        -- [[ Asphodel Miniboss Counts ]]
+        Asphodel = {
+            B_MiniBoss01 = {
+                MaxCreationsThisRun = config.Asphodel_PowerCouple,
+            },
+            B_MiniBoss02 = {
+                MaxCreationsThisRun = config.Asphodel_Witches,
+            },
+            B_Wrapping01 = {
+                MaxCreationsThisRun = config.Asphodel_Barge,
+            },
+        },
+        -- [[ Elysium Miniboss Counts ]]
+        Elysium = {
+            C_MiniBoss01 = {
+                MaxCreationsThisRun = config.Elysium_Asterius,
+            },
+            C_MiniBoss02 = {
+                MaxCreationsThisRun = config.Elysium_ButterflyBall,
+            },
+        },
+    })
+
+    -- Remove Tiny Vermin
+    if config.RemoveTinyVermin then
+        ModUtil.MapSetTable(RoomSetData.Styx.D_MiniBoss03, {
+            LegalEncounters = { "MiniBossHeavyRangedForked" },
+        })
+    end
+end)
+
+-- Scripts/RunManager.lua : 515
+ModUtil.WrapBaseFunction("ChooseNextRoomData", function( baseFunc, currentRun, args )
+    -- IsRoomEligible looks at RoomCreations and ignores it if it's nil. Make sure it's not nil
+    -- Easier and cleaner than overriding the IsRoomEligible function to fix nil behavior
+    ModUtil.MapSetTable(CurrentRun.RoomCreations, {
+        A_MiniBoss01 = currentRun.RoomCreations.A_MiniBoss01 or 0,
+        A_MiniBoss02 = currentRun.RoomCreations.A_MiniBoss02 or 0,
+        A_MiniBoss03 = currentRun.RoomCreations.A_MiniBoss03 or 0,
+        A_MiniBoss04 = currentRun.RoomCreations.A_MiniBoss04 or 0,
+        B_MiniBoss01 = currentRun.RoomCreations.B_MiniBoss01 or 0,
+        B_MiniBoss02 = currentRun.RoomCreations.B_MiniBoss02 or 0,
+        B_Wrapping01 = currentRun.RoomCreations.B_Wrapping01 or 0,
+        C_MiniBoss01 = currentRun.RoomCreations.C_MiniBoss01 or 0,
+        C_MiniBoss02 = currentRun.RoomCreations.C_MiniBoss02 or 0,
+    })
+
+    return baseFunc(currentRun, args)
+end, MinibossControl)
+
+-- Scripts/RoomManager.lua : 1874
+ModUtil.WrapBaseFunction("StartRoom", function ( baseFunc, currentRun, currentRoom )
+    PrintUtil.showModdedWarning()
+
+    baseFunc(currentRun, currentRoom)
+end, MinibossControl)
+
+-- Scripts/UIScripts.lua : 145
+ModUtil.WrapBaseFunction("ShowCombatUI", function ( baseFunc, flag )
+    PrintUtil.showModdedWarning()
+
+    baseFunc(flag)
+end, MinibossControl)
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # MinibossControl
-Change amount of times each Miniboss can spawn in Tartarus, Asphodel, and Elysium. Also can toggle TinyVermin.
+This mod is used to change the proportions of miniboss chambers, allowing minibosses to be replaced with others. For example, Barge can be removed and replaced with Witches, which means that two doors would be Witches instead of one.
+
+**This is not [leaderboard](https://speedrun.com/hades) legal, and any runs found to be using this mod _will_ be rejected** 
+
+# Installation
+To install MinibossControl, go the the [Releases](https://github.com/Museus/MinibossControl/releases) tab and download the latest .zip file. This will include the following files:
+
+-   Mods/
+    - MinibossControl/
+    - ModUtil/
+    - PrintUtil/
+-   modimporter.py
+
+[A video tutorial on how to install mods is available from PonyWarrior here](https://www.youtube.com/watch?v=YF0ij7MgOrI)
+
+If you prefer text instructions, follow these steps:
+
+If you don't already have Python installed, download it from [python.org](https://www.python.org/downloads/) and install it.
+
+Once you have downloaded the `MinibossControl.zip` file, open up your Hades game directory. You can find this by launching Hades, then opening Task Manager, finding the Hades process, right-clicking on it, and selecting Open File Location.
+
+Unzip the files into the `.../Hades/Content` folder. You should now have the standard folders such as `Scripts` and `Game` as well as a new folder called `Mods` and the `modimporter.py` script.
+
+Run the `modimporter.py` script to install the mods, then load into your game. Whenever you want to uninstall the mods, simply delete the contents of the `Mods` folder, and run the `modimporter.py` script again.
+
+# Configuration
+
+You can set the Creation count for each miniboss in Tartarus, Asphodel, and Elysium, and remove Tiny Vermin. To change these options, open the `Mods` folder,  open the `MinibossControl` folder, then open `MinibossControl.lua` in Notepad. There will be a block at the top with `local config = {` followed by options that can be changed.
